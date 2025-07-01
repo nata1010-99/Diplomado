9141f326f09c28dff0272e5bd709dc57db2ed9e8
Natalia Morales <natamoralesv@gmail.com>
2025-07-01 11:56:59 -0500

    Semana_2

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+from cargar_datos import show_data_tab
+from transformacion import show_transform_tab
+from visualizaciones import show_visualization_tab
+
+# Crear pestañas en el cuerpo de la aplicación
+tabs = st.tabs(["📥 Carga de Datos", "🔧 Transformación y Métricas", "📊 Visualizaciones", "🗺️ Mapa"])
+
+# Mostrar contenido en cada pestaña
+with tabs[0]:
+    show_data_tab()
+
+with tabs[1]:
+    show_transform_tab()
+
+with tabs[2]:
+    show_visualization_tab()
+
+with tabs[3]:
+    st.subheader("🗺️ Mapa")
+    st.write("Aquí irá el contenido del mapa.")
+
+
+
+
+
+
+
